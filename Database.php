9c40543d9bdaead3419b0dd9b6aa40e12cb6e9c6
Loucman <loucman.machababy@epitech.eu>
2025-01-27 11:58:28 +0100

    Creation de linterface de connexion, de linterface dinscription, et de la BDD

---
+++ Database.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST','localhost');
+define('DB_NAME', 'Afrika');
+define('USER', 'phpmyadmin');
+define('PASS', 'Mouslime74');
+
+try {
+    
+} catch(PDOException $e) {
+    echo 'error '
+}
